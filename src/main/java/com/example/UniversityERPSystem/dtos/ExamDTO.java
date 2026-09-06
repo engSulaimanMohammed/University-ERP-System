e15bb8804f080522c1e0b784b93505cd40706fc0
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 18:49:26 +0400

    feat: add Exam DTO structure

--- src/main/java/com/example/UniversityERPSystem/dtos/ExamDTO.java
+++ src/main/java/com/example/UniversityERPSystem/dtos/ExamDTO.java
@@ -1,4 +1,25 @@
 package com.example.UniversityERPSystem.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ExamDTO {
+
+    private Long id;
+
+    private String title;
+
+    private Date examDate;
+
+    private double totalMarks;
+
+    private Long courseId;
 }
