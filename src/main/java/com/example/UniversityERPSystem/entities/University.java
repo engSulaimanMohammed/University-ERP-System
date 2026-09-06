eee7a3d864fa1e0e85fcd4d42367431b7891b588
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 09:51:11 +0400

    feat: add University Faculty relationship

--- src/main/java/com/example/UniversityERPSystem/entities/University.java
+++ src/main/java/com/example/UniversityERPSystem/entities/University.java
@@ -1,10 +1,16 @@
 package com.example.UniversityERPSystem.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
 
 @Entity
 public class University extends BaseClass {
 
     private String name;
     private String location;
+
+    @OneToMany(mappedBy = "university")
+    private List<Faculty> faculties;
 }
