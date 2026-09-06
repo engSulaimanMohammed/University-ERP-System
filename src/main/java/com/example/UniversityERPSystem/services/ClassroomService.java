133747b44afdfc8bf15348bc1f6d97fcb3b18dd0
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 15:13:20 +0400

    feat: add Classroom service

--- src/main/java/com/example/UniversityERPSystem/services/ClassroomService.java
+++ src/main/java/com/example/UniversityERPSystem/services/ClassroomService.java
@@ -1,4 +1,21 @@
 package com.example.UniversityERPSystem.services;
 
+
+
+import com.example.UniversityERPSystem.repositories.ClassroomRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ClassroomService {
+
+    private final ClassroomRepository classroomRepository;
+
+    public ClassroomService(ClassroomRepository classroomRepository) {
+        this.classroomRepository = classroomRepository;
+    }
+
+
+
+
+
 }
