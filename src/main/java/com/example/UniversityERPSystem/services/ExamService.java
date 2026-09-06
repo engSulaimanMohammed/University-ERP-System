7c5f001d2f86d00dd7f0b2c844fc95332f23bf9f
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 15:01:04 +0400

    feat: add Exam service

--- src/main/java/com/example/UniversityERPSystem/services/ExamService.java
+++ src/main/java/com/example/UniversityERPSystem/services/ExamService.java
@@ -1,4 +1,19 @@
 package com.example.UniversityERPSystem.services;
 
+
+import com.example.UniversityERPSystem.repositories.ExamRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ExamService {
+
+    private final ExamRepository examRepository;
+
+    public ExamService(ExamRepository examRepository) {
+        this.examRepository = examRepository;
+    }
+
+
+
+
 }
