34101ecbbefd160b52cd6c1ee2cc22aac7925a01
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 16:24:48 +0400

    feat: add Course controller

--- src/main/java/com/example/UniversityERPSystem/controllers/CourseController.java
+++ src/main/java/com/example/UniversityERPSystem/controllers/CourseController.java
@@ -1,4 +1,24 @@
 package com.example.UniversityERPSystem.controllers;
 
+import com.example.UniversityERPSystem.entities.Course;
+import com.example.UniversityERPSystem.services.CourseService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/course")
 public class CourseController {
+
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+
+
+
+
+
 }
