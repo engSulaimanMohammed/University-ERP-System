7734478f91d7046280d6fb1ff047fa9ae4a08f97
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 14:33:06 +0400

    feat: add Department create method

--- src/main/java/com/example/UniversityERPSystem/services/DepartmentService.java
+++ src/main/java/com/example/UniversityERPSystem/services/DepartmentService.java
@@ -1,8 +1,12 @@
 package com.example.UniversityERPSystem.services;
 
 
+import com.example.UniversityERPSystem.entities.Department;
+import com.example.UniversityERPSystem.entities.Faculty;
 import com.example.UniversityERPSystem.repositories.DepartmentRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class DepartmentService {
@@ -12,6 +16,23 @@ public class DepartmentService {
     public DepartmentService(DepartmentRepository departmentRepository) {
         this.departmentRepository = departmentRepository;
     }
+
+
+    public Department addDepartment(Department department, Faculty faculty) {
+        if (department == null) {
+            throw new IllegalArgumentException("Department cannot be null");
+        }
+        department.setActive(true);
+        department.setCreatedDate(new Date());
+        department.setFaculty(faculty);
+        return departmentRepository.save(department);
+    }
+
+
+
+
+
+
 
 
 
