58ab183d56ce9b195f30ef617347cf9a6c6abbb1
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 10:42:58 +0400

    feat: add Guardian entity

--- src/main/java/com/example/UniversityERPSystem/entities/Guardian.java
+++ src/main/java/com/example/UniversityERPSystem/entities/Guardian.java
@@ -1,4 +1,15 @@
 package com.example.UniversityERPSystem.entities;
 
-public class Guardian {
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Guardian extends BaseClass {
+
+    private String name;
+    private String relationship;
+    private String phoneNumber;
+
+    @ManyToOne
+    private Student student;
 }
