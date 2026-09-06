7731af046cde609b95921baf7dc1c1f4ac3eb1b3
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 15:09:44 +0400

    feat: add Guardian service

--- src/main/java/com/example/UniversityERPSystem/services/GuardianService.java
+++ src/main/java/com/example/UniversityERPSystem/services/GuardianService.java
@@ -1,4 +1,16 @@
 package com.example.UniversityERPSystem.services;
 
+
+
+import com.example.UniversityERPSystem.repositories.GuardianRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class GuardianService {
+
+    private final GuardianRepository guardianRepository;
+
+    public GuardianService(GuardianRepository guardianRepository) {
+        this.guardianRepository = guardianRepository;
+    }
 }
