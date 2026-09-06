ab0c7e519b08ba3322e66081757c697d3a08995d
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 16:26:13 +0400

    feat: add Course update endpoint

--- src/main/java/com/example/UniversityERPSystem/controllers/CourseController.java
+++ src/main/java/com/example/UniversityERPSystem/controllers/CourseController.java
@@ -39,6 +39,16 @@ public class CourseController {
     }
 
 
+    @PutMapping("/update/{id}")
+    public Course updateCourse(@PathVariable Long id,
+                               @RequestBody Course course) {
+        return null;
+    }
+
+
+
+
+
 
 
 
