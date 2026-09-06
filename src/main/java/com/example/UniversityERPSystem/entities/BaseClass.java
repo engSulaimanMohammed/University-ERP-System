5e8a6f4ddf16bdf4c8b4a7f76105a4371efa43d2
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 09:39:34 +0400

    feat: add shared BaseClass entity fields

--- src/main/java/com/example/UniversityERPSystem/entities/BaseClass.java
+++ src/main/java/com/example/UniversityERPSystem/entities/BaseClass.java
@@ -1,4 +1,20 @@
 package com.example.UniversityERPSystem.entities;
 
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+import java.util.Date;
+
+@MappedSuperclass
 public class BaseClass {
+
+    @Id
+    @GeneratedValue
+
+    private Long id;
+    private boolean isActive;
+    private Date createdDate;
+    private Date updatedDate;
 }
