2e41c69bdaa7a7c32ef7c5cfaaeda8f16e4a6e53
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 18:37:25 +0400

    feat: add Department DTO validation

--- src/main/java/com/example/UniversityERPSystem/dtos/DepartmentDTO.java
+++ src/main/java/com/example/UniversityERPSystem/dtos/DepartmentDTO.java
@@ -1,5 +1,8 @@
 package com.example.UniversityERPSystem.dtos;
-
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,9 +16,18 @@ public class DepartmentDTO {
 
     private Long id;
 
+
+    @NotBlank(message = "Department name cannot be blank")
+    @Size(max = 255, message = "Department name cannot exceed 255 characters")
     private String name;
 
+
+    @NotBlank(message = "Department description cannot be blank")
+    @Size(max = 255, message = "Department description cannot exceed 255 characters")
     private String description;
 
+
+    @NotNull(message = "Faculty ID cannot be null")
+    @Positive(message = "Faculty ID must be greater than zero")
     private Long facultyId;
 }
