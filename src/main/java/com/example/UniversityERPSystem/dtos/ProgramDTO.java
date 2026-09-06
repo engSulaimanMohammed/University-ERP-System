ab1a0e856a92e9a85f73bb2f99504b4b73be9b35
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 18:39:50 +0400

    feat: add Program DTO structure

--- src/main/java/com/example/UniversityERPSystem/dtos/ProgramDTO.java
+++ src/main/java/com/example/UniversityERPSystem/dtos/ProgramDTO.java
@@ -1,4 +1,23 @@
 package com.example.UniversityERPSystem.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProgramDTO {
+
+    private Long id;
+
+    private String name;
+
+    private String degreeLevel;
+
+    private int durationYears;
+
+    private Long departmentId;
 }
