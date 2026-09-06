9d72b6066e076d3482ddb1c1a885c0b65cd832dd
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 12:37:43 +0400

    feat: add University create and get all methods

--- src/main/java/com/example/UniversityERPSystem/services/UniversityService.java
+++ src/main/java/com/example/UniversityERPSystem/services/UniversityService.java
@@ -4,6 +4,9 @@ import com.example.UniversityERPSystem.entities.University;
 import com.example.UniversityERPSystem.repositories.UniversityRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class UniversityService {
 
@@ -17,6 +20,20 @@ public class UniversityService {
         if (university == null) {
             throw new IllegalArgumentException("University cannot be null");
         }
+        university.setActive(true);
         return universityRepository.save(university);
     }
+
+
+    public List<University> getAllUniversities() {
+        List<University> universities = universityRepository.findAll();
+        List<University> activeUniversities = new ArrayList<>();
+        for (University university : universities) {
+            if (university.isActive()) {
+                activeUniversities.add(university);
+            }
+        }
+        return activeUniversities;
+    }
+
 }
