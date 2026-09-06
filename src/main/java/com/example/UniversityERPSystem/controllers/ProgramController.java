6193cb4710b0a4b97134cdb068176039fe49a6d3
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 16:21:37 +0400

    feat: add Program controller

--- src/main/java/com/example/UniversityERPSystem/controllers/ProgramController.java
+++ src/main/java/com/example/UniversityERPSystem/controllers/ProgramController.java
@@ -1,4 +1,22 @@
 package com.example.UniversityERPSystem.controllers;
 
+import com.example.UniversityERPSystem.entities.Program;
+import com.example.UniversityERPSystem.services.ProgramService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/program")
 public class ProgramController {
+
+    private final ProgramService programService;
+
+    public ProgramController(ProgramService programService) {
+        this.programService = programService;
+    }
+
+
+
+
 }
