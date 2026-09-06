c13741cb28238e60ee1c52fc3b5d4f90b8fdf04d
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 16:34:30 +0400

    feat: add Enrollment controller

--- src/main/java/com/example/UniversityERPSystem/controllers/EnrollmentController.java
+++ src/main/java/com/example/UniversityERPSystem/controllers/EnrollmentController.java
@@ -1,4 +1,21 @@
 package com.example.UniversityERPSystem.controllers;
 
+import com.example.UniversityERPSystem.entities.Enrollment;
+import com.example.UniversityERPSystem.services.EnrollmentService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/enrollment")
 public class EnrollmentController {
+
+    private final EnrollmentService enrollmentService;
+
+    public EnrollmentController(EnrollmentService enrollmentService) {
+        this.enrollmentService = enrollmentService;
+    }
+
+
+
 }
