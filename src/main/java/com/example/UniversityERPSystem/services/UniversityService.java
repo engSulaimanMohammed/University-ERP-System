d2c54d0f3edfa567332a5d3fca89cc3b6f77a8a8
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 11:51:41 +0400

    feat: add University service

--- src/main/java/com/example/UniversityERPSystem/services/UniversityService.java
+++ src/main/java/com/example/UniversityERPSystem/services/UniversityService.java
@@ -1,4 +1,14 @@
 package com.example.UniversityERPSystem.services;
 
+import com.example.UniversityERPSystem.repositories.UniversityRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UniversityService {
+
+    private final UniversityRepository universityRepository;
+
+    public UniversityService(UniversityRepository universityRepository) {
+        this.universityRepository = universityRepository;
+    }
 }
