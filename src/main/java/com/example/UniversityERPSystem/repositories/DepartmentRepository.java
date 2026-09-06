7b6210e70150dbbcdb5277f3d6fe4c5dce86cf82
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 11:39:41 +0400

    feat: add Department repository

--- src/main/java/com/example/UniversityERPSystem/repositories/DepartmentRepository.java
+++ src/main/java/com/example/UniversityERPSystem/repositories/DepartmentRepository.java
@@ -1,4 +1,7 @@
 package com.example.UniversityERPSystem.repositories;
 
-public interface DepartmentRepository {
+import com.example.UniversityERPSystem.entities.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
 }
