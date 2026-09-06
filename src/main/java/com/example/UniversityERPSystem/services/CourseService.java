c4586e73a24c8e404b61d49387bbe514ea1e952d
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 14:45:20 +0400

    feat: add Course create method

--- src/main/java/com/example/UniversityERPSystem/services/CourseService.java
+++ src/main/java/com/example/UniversityERPSystem/services/CourseService.java
@@ -1,8 +1,13 @@
 package com.example.UniversityERPSystem.services;
 
 
+import com.example.UniversityERPSystem.entities.Course;
+import com.example.UniversityERPSystem.entities.Instructor;
+import com.example.UniversityERPSystem.entities.Program;
 import com.example.UniversityERPSystem.repositories.CourseRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class CourseService {
@@ -12,6 +17,23 @@ public class CourseService {
     public CourseService(CourseRepository courseRepository) {
         this.courseRepository = courseRepository;
     }
+
+
+    public Course addCourse(Course course, Program program, Instructor instructor) {
+        if (course == null) {
+            throw new IllegalArgumentException("Course cannot be null");
+        }
+        course.setActive(true);
+        course.setCreatedDate(new Date());
+        course.setProgram(program);
+        course.setInstructor(instructor);
+        return courseRepository.save(course);
+    }
+
+
+
+
+
 
 
 
