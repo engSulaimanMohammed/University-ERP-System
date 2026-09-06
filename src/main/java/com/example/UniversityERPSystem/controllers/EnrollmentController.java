dfcceeeace788114f2c1700e1210fe23d627cefa
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 18:05:41 +0400

    fix: connect Enrollment CRUD endpoints with service

--- src/main/java/com/example/UniversityERPSystem/controllers/EnrollmentController.java
+++ src/main/java/com/example/UniversityERPSystem/controllers/EnrollmentController.java
@@ -19,32 +19,42 @@ public class EnrollmentController {
 
     @PostMapping("/add")
     public Enrollment addEnrollment(@RequestBody Enrollment enrollment) {
-        return null;
+        return enrollmentService.addEnrollment(
+                enrollment,
+                enrollment.getStudent(),
+                enrollment.getCourse()
+        );
     }
 
 
 
     @GetMapping("/getAll")
     public List<Enrollment> getAllEnrollments() {
-        return null;
+        return enrollmentService.getAllEnrollments();
     }
 
 
     @GetMapping("/getById/{id}")
     public Enrollment getById(@PathVariable Long id) {
-        return null;
+        return enrollmentService.getById(id);
     }
 
 
     @PutMapping("/update/{id}")
     public Enrollment updateEnrollment(@PathVariable Long id,
                                        @RequestBody Enrollment enrollment) {
-        return null;
+        return enrollmentService.updateEnrollment(
+                id,
+                enrollment.getEnrollmentDate(),
+                enrollment.getStatus(),
+                enrollment.getStudent(),
+                enrollment.getCourse()
+        );
     }
 
 
     @DeleteMapping("/delete/{id}")
     public Boolean deleteById(@PathVariable Long id) {
-        return null;
+        return enrollmentService.deleteById(id);
     }
 }
