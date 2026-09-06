ff420c2c5b4c4c28f11ccd7ef47ab630540ba0cf
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 19:44:58 +0400

    feat: add Student grade statistics DTO

---
+++ src/main/java/com/example/UniversityERPSystem/dtos/StudentGradeStatsDTO.java
@@ -0,0 +1,19 @@
+package com.example.UniversityERPSystem.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentGradeStatsDTO {
+
+    private Long studentId;
+
+    private double averageScore;
+
+    private String gpaClassification;
+}
