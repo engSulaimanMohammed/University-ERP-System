b103adb931b39200e0f4a1cbcd41acf69a4866c5
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 16:37:45 +0400

    feat: add Exam controller

--- src/main/java/com/example/UniversityERPSystem/controllers/ExamController.java
+++ src/main/java/com/example/UniversityERPSystem/controllers/ExamController.java
@@ -1,4 +1,21 @@
 package com.example.UniversityERPSystem.controllers;
 
+import com.example.UniversityERPSystem.entities.Exam;
+import com.example.UniversityERPSystem.services.ExamService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/exam")
 public class ExamController {
+
+    private final ExamService examService;
+
+    public ExamController(ExamService examService) {
+        this.examService = examService;
+    }
+
+
+
 }
