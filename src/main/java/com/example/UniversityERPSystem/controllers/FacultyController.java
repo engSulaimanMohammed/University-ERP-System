c396c375ff14ae0b0b345dcad363355fe0f4330a
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 15:30:52 +0400

    feat: add get all Faculties endpoint

--- src/main/java/com/example/UniversityERPSystem/controllers/FacultyController.java
+++ src/main/java/com/example/UniversityERPSystem/controllers/FacultyController.java
@@ -24,6 +24,13 @@ public class FacultyController {
     }
 
 
+    @GetMapping("/getAll")
+    public List<Faculty> getAllFaculties() {
+        // Call getAllFaculties.
+        return null;
+    }
+
+
 
 
 
