89ffcb36ac8f2800abdd070f487cfe9e8b573713
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 15:30:11 +0400

    feat: add Faculty controller

--- src/main/java/com/example/UniversityERPSystem/controllers/FacultyController.java
+++ src/main/java/com/example/UniversityERPSystem/controllers/FacultyController.java
@@ -1,4 +1,24 @@
 package com.example.UniversityERPSystem.controllers;
 
+
+
+import com.example.UniversityERPSystem.entities.Faculty;
+import com.example.UniversityERPSystem.services.FacultyService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/faculty")
 public class FacultyController {
+
+    private final FacultyService facultyService;
+
+    public FacultyController(FacultyService facultyService) {
+        this.facultyService = facultyService;
+    }
+
+
+
+
 }
