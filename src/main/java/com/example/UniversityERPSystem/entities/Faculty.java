93dd253c5b023505fc70334a89bede14a3d95107
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 11:12:26 +0400

    fix: prevent University Faculty cyclic reference

--- src/main/java/com/example/UniversityERPSystem/entities/Faculty.java
+++ src/main/java/com/example/UniversityERPSystem/entities/Faculty.java
@@ -1,5 +1,6 @@
 package com.example.UniversityERPSystem.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
@@ -12,6 +13,7 @@ public class Faculty extends BaseClass {
     private String name;
     private String description;
 
+    @JsonIgnore
     @ManyToOne
     private University university;
 
