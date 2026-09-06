d9d41b03cd4117687221837ab97ff2d186027614
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 18:47:25 +0400

    feat: add Enrollment DTO structure

--- src/main/java/com/example/UniversityERPSystem/dtos/EnrollmentDTO.java
+++ src/main/java/com/example/UniversityERPSystem/dtos/EnrollmentDTO.java
@@ -1,4 +1,25 @@
 package com.example.UniversityERPSystem.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class EnrollmentDTO {
+
+    private Long id;
+
+    private Date enrollmentDate;
+
+    private String status;
+
+    private Long studentId;
+
+    private Long courseId;
 }
