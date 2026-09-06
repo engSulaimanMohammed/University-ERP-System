1d5c517a6c82c2b837a6d26422389bd37a35061e
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 09:55:02 +0400

    feat: add Faculty entity

--- src/main/java/com/example/UniversityERPSystem/entities/Faculty.java
+++ src/main/java/com/example/UniversityERPSystem/entities/Faculty.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 
 @Entity
-public class Faculty {
+public class Faculty extends BaseClass {
 
     private String name;
     private String description;
