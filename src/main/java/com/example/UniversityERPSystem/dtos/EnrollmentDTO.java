8c80307dbfaa8bf7aa96cdffdce587843d9a5c95
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 18:47:41 +0400

    feat: add Enrollment DTO validation

--- src/main/java/com/example/UniversityERPSystem/dtos/EnrollmentDTO.java
+++ src/main/java/com/example/UniversityERPSystem/dtos/EnrollmentDTO.java
@@ -1,5 +1,11 @@
 package com.example.UniversityERPSystem.dtos;
 
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,11 +21,25 @@ public class EnrollmentDTO {
 
     private Long id;
 
+
+    @NotNull(message = "Enrollment date cannot be null")
+    @PastOrPresent(message = "Enrollment date cannot be in the future")
     private Date enrollmentDate;
 
+
+    @NotBlank(message = "Enrollment status cannot be blank")
+    @Size(max = 255, message = "Enrollment status cannot exceed 255 characters")
     private String status;
 
+
+    @NotNull(message = "Student ID cannot be null")
+    @Positive(message = "Student ID must be greater than zero")
     private Long studentId;
 
+
+    @NotNull(message = "Course ID cannot be null")
+    @Positive(message = "Course ID must be greater than zero")
     private Long courseId;
+
+
 }
