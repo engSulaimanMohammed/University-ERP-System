c9d30ac0e1102bc324a78dc99605340b61af9535
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 14:48:52 +0400

    feat: add Instructor service

--- src/main/java/com/example/UniversityERPSystem/services/InstructorService.java
+++ src/main/java/com/example/UniversityERPSystem/services/InstructorService.java
@@ -1,4 +1,18 @@
 package com.example.UniversityERPSystem.services;
 
+
+import com.example.UniversityERPSystem.repositories.InstructorRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class InstructorService {
+
+    private final InstructorRepository instructorRepository;
+
+    public InstructorService(InstructorRepository instructorRepository) {
+        this.instructorRepository = instructorRepository;
+    }
+
+
+
 }
