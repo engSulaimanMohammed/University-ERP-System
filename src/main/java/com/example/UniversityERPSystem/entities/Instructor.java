8b58298630ce37f87323aa8678069a7283e728f5
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 10:24:19 +0400

    feat: add Instructor entity

--- src/main/java/com/example/UniversityERPSystem/entities/Instructor.java
+++ src/main/java/com/example/UniversityERPSystem/entities/Instructor.java
@@ -1,4 +1,19 @@
 package com.example.UniversityERPSystem.entities;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Instructor {
+
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String specialization;
+
+    @ManyToOne
+    private Department department;
+
+
 }
