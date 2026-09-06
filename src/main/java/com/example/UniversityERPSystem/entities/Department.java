27c7d60ee0c84f993d4b6c5d2993b17a6638c1f8
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 10:01:10 +0400

    feat: add Department Program relationship

--- src/main/java/com/example/UniversityERPSystem/entities/Department.java
+++ src/main/java/com/example/UniversityERPSystem/entities/Department.java
@@ -3,6 +3,9 @@ package com.example.UniversityERPSystem.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
 
 @Entity
 public class Department extends BaseClass {
@@ -12,4 +15,7 @@ public class Department extends BaseClass {
 
     @ManyToOne
     private Faculty faculty;
+
+    @OneToMany(mappedBy = "department")
+    private List<Program> programs;
 }
