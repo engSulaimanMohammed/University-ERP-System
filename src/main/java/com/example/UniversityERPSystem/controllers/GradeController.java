60558fba1080e7a5b3e382d21c4728e441f22154
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 16:40:58 +0400

    feat: add Grade controller

--- src/main/java/com/example/UniversityERPSystem/controllers/GradeController.java
+++ src/main/java/com/example/UniversityERPSystem/controllers/GradeController.java
@@ -1,4 +1,22 @@
 package com.example.UniversityERPSystem.controllers;
 
+import com.example.UniversityERPSystem.entities.Grade;
+import com.example.UniversityERPSystem.services.GradeService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/grade")
 public class GradeController {
+
+    private final GradeService gradeService;
+
+    public GradeController(GradeService gradeService) {
+        this.gradeService = gradeService;
+    }
+
+
+
+
 }
