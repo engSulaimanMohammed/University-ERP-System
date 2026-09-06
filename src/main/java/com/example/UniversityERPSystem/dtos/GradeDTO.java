fc0f7e23cdec576c3fd5f76343f1827c97949373
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 18:51:02 +0400

    feat: add Grade DTO structure

--- src/main/java/com/example/UniversityERPSystem/dtos/GradeDTO.java
+++ src/main/java/com/example/UniversityERPSystem/dtos/GradeDTO.java
@@ -1,4 +1,23 @@
 package com.example.UniversityERPSystem.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class GradeDTO {
+
+    private Long id;
+
+    private double score;
+
+    private String letterGrade;
+
+    private Long enrollmentId;
+
+    private Long examId;
 }
