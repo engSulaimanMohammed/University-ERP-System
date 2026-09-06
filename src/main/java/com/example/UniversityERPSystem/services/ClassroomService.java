911b30ffce93cfef1b2ce6e9a9c635a364add122
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 15:14:06 +0400

    feat: add Classroom create method

--- src/main/java/com/example/UniversityERPSystem/services/ClassroomService.java
+++ src/main/java/com/example/UniversityERPSystem/services/ClassroomService.java
@@ -2,8 +2,12 @@ package com.example.UniversityERPSystem.services;
 
 
 
+import com.example.UniversityERPSystem.entities.Classroom;
+import com.example.UniversityERPSystem.entities.Department;
 import com.example.UniversityERPSystem.repositories.ClassroomRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class ClassroomService {
@@ -13,6 +17,22 @@ public class ClassroomService {
     public ClassroomService(ClassroomRepository classroomRepository) {
         this.classroomRepository = classroomRepository;
     }
+
+
+
+    public Classroom addClassroom(Classroom classroom, Department department) {
+        if (classroom == null) {
+            throw new IllegalArgumentException("Classroom cannot be null");
+        }
+        classroom.setActive(true);
+        classroom.setCreatedDate(new Date());
+        classroom.setDepartment(department);
+        return classroomRepository.save(classroom);
+    }
+
+
+
+
 
 
 
