eaac2f94664a083ca8c57230a60e3269994c0e91
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 14:57:21 +0400

    feat: add Enrollment create method

--- src/main/java/com/example/UniversityERPSystem/services/EnrollmentService.java
+++ src/main/java/com/example/UniversityERPSystem/services/EnrollmentService.java
@@ -1,7 +1,12 @@
 package com.example.UniversityERPSystem.services;
 
+import com.example.UniversityERPSystem.entities.Course;
+import com.example.UniversityERPSystem.entities.Enrollment;
+import com.example.UniversityERPSystem.entities.Student;
 import com.example.UniversityERPSystem.repositories.EnrollmentRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class EnrollmentService {
@@ -11,6 +16,28 @@ public class EnrollmentService {
     public EnrollmentService(EnrollmentRepository enrollmentRepository) {
         this.enrollmentRepository = enrollmentRepository;
     }
+
+
+
+    public Enrollment addEnrollment(Enrollment enrollment, Student student, Course course) {
+        if (enrollment == null) {
+            throw new IllegalArgumentException("Enrollment cannot be null");
+        }
+        enrollment.setActive(true);
+        enrollment.setCreatedDate(new Date());
+        enrollment.setStudent(student);
+        enrollment.setCourse(course);
+        return enrollmentRepository.save(enrollment);
+    }
+
+
+
+
+
+
+
+
+
 
 
 
