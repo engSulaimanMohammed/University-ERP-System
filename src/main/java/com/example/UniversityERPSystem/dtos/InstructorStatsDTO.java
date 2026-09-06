4ec8a96f0327adc88145cca3215229fcc0a422b0
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 19:32:23 +0400

    feat: add Instructor statistics DTO

--- src/main/java/com/example/UniversityERPSystem/dtos/InstructorStatsDTO.java
+++ src/main/java/com/example/UniversityERPSystem/dtos/InstructorStatsDTO.java
@@ -1,4 +1,19 @@
 package com.example.UniversityERPSystem.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class InstructorStatsDTO {
+
+    private Long instructorId;
+
+    private String instructorName;
+
+    private int totalCoursesTaught;
 }
