6c3cc5a5e2d10a697012b995cf849c9145c4c6c0
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 15:10:20 +0400

    feat: add Guardian create method

--- src/main/java/com/example/UniversityERPSystem/services/GuardianService.java
+++ src/main/java/com/example/UniversityERPSystem/services/GuardianService.java
@@ -2,8 +2,12 @@ package com.example.UniversityERPSystem.services;
 
 
 
+import com.example.UniversityERPSystem.entities.Guardian;
+import com.example.UniversityERPSystem.entities.Student;
 import com.example.UniversityERPSystem.repositories.GuardianRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class GuardianService {
@@ -13,4 +17,25 @@ public class GuardianService {
     public GuardianService(GuardianRepository guardianRepository) {
         this.guardianRepository = guardianRepository;
     }
+
+
+
+    public Guardian addGuardian(Guardian guardian, Student student) {
+        if (guardian == null) {
+            throw new IllegalArgumentException("Guardian cannot be null");
+        }
+        guardian.setActive(true);
+        guardian.setCreatedDate(new Date());
+        guardian.setStudent(student);
+        return guardianRepository.save(guardian);
+    }
+
+
+
+
+
+
+
+
+
 }
