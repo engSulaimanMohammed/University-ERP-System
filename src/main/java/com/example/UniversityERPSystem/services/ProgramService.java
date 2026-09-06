0f551db1cdff36159557f4787904f6df46f3a86e
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 14:37:21 +0400

    feat: add Program create method

--- src/main/java/com/example/UniversityERPSystem/services/ProgramService.java
+++ src/main/java/com/example/UniversityERPSystem/services/ProgramService.java
@@ -1,7 +1,10 @@
 package com.example.UniversityERPSystem.services;
+import com.example.UniversityERPSystem.entities.Department;
+import com.example.UniversityERPSystem.entities.Program;
 import com.example.UniversityERPSystem.repositories.ProgramRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 
 
 @Service
@@ -12,6 +15,20 @@ public class ProgramService {
     public ProgramService(ProgramRepository programRepository) {
         this.programRepository = programRepository;
     }
+
+
+
+    public Program addProgram(Program program, Department department) {
+        if (program == null) {
+            throw new IllegalArgumentException("Program cannot be null");
+        }
+        program.setActive(true);
+        program.setCreatedDate(new Date());
+        program.setDepartment(department);
+        return programRepository.save(program);
+    }
+
+
 
 
 
