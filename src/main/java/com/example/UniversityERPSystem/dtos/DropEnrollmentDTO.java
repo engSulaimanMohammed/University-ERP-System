5eeb5956c087eee1d7da0fd825a2c34f9b9987d9
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 20:08:18 +0400

    feat: add enrollment drop request DTO

--- src/main/java/com/example/UniversityERPSystem/dtos/DropEnrollmentDTO.java
+++ src/main/java/com/example/UniversityERPSystem/dtos/DropEnrollmentDTO.java
@@ -1,4 +1,19 @@
 package com.example.UniversityERPSystem.dtos;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DropEnrollmentDTO {
+
+    @NotNull(message = "Enrollment ID cannot be null")
+    @Positive(message = "Enrollment ID must be greater than zero")
+    private Long enrollmentId;
 }
