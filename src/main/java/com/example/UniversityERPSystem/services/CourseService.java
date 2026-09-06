32da8497e09a50ef25b3d7394d16aa9ad3199d8b
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 14:44:24 +0400

    feat: add Course service

--- src/main/java/com/example/UniversityERPSystem/services/CourseService.java
+++ src/main/java/com/example/UniversityERPSystem/services/CourseService.java
@@ -1,4 +1,18 @@
 package com.example.UniversityERPSystem.services;
 
+
+import com.example.UniversityERPSystem.repositories.CourseRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CourseService {
+
+    private final CourseRepository courseRepository;
+
+    public CourseService(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+
+
 }
