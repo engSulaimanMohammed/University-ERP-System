624b52818e5ff431422560b3ad0de9d6e66eb891
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 18:12:16 +0400

    fix: connect Guardian CRUD endpoints with service

--- src/main/java/com/example/UniversityERPSystem/controllers/GuardianController.java
+++ src/main/java/com/example/UniversityERPSystem/controllers/GuardianController.java
@@ -19,32 +19,41 @@ public class GuardianController {
 
     @PostMapping("/add")
     public Guardian addGuardian(@RequestBody Guardian guardian) {
-        return null;
+        return guardianService.addGuardian(
+                guardian,
+                guardian.getStudent()
+        );
     }
 
 
     @GetMapping("/getAll")
     public List<Guardian> getAllGuardians() {
-        return null;
+        return guardianService.getAllGuardians();
     }
 
 
     @GetMapping("/getById/{id}")
     public Guardian getById(@PathVariable Long id) {
-        return null;
+        return guardianService.getById(id);
     }
 
 
     @PutMapping("/update/{id}")
     public Guardian updateGuardian(@PathVariable Long id,
                                    @RequestBody Guardian guardian) {
-        return null;
+        return guardianService.updateGuardian(
+                id,
+                guardian.getName(),
+                guardian.getRelationship(),
+                guardian.getPhoneNumber(),
+                guardian.getStudent()
+        );
     }
 
 
     @DeleteMapping("/delete/{id}")
     public Boolean deleteById(@PathVariable Long id) {
-        return null;
+        return guardianService.deleteById(id);
     }
 }
 
