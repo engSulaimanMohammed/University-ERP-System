8eb7a1e7252e0be5abad159182b57e53e048767d
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 16:18:04 +0400

    feat: add Department controller

--- src/main/java/com/example/UniversityERPSystem/controllers/DepartmentController.java
+++ src/main/java/com/example/UniversityERPSystem/controllers/DepartmentController.java
@@ -1,4 +1,20 @@
 package com.example.UniversityERPSystem.controllers;
 
+import com.example.UniversityERPSystem.entities.Department;
+import com.example.UniversityERPSystem.services.DepartmentService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/department")
 public class DepartmentController {
+
+    private final DepartmentService departmentService;
+
+    public DepartmentController(DepartmentService departmentService) {
+        this.departmentService = departmentService;
+    }
+
+
 }
