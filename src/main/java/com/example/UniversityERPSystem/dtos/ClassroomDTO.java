24dfb66b22d0712c427f395f959e7ecf8fb2e1d9
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 18:55:13 +0400

    feat: add Classroom DTO structure

--- src/main/java/com/example/UniversityERPSystem/dtos/ClassroomDTO.java
+++ src/main/java/com/example/UniversityERPSystem/dtos/ClassroomDTO.java
@@ -1,4 +1,23 @@
 package com.example.UniversityERPSystem.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClassroomDTO {
+
+    private Long id;
+
+    private String roomNumber;
+
+    private int floor;
+
+    private int capacity;
+
+    private Long departmentId;
 }
