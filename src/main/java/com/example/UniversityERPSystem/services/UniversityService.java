99c7f6bdb919401681b5f6a48068d26719918261
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 12:26:10 +0400

    feat: add University create service method

--- src/main/java/com/example/UniversityERPSystem/services/UniversityService.java
+++ src/main/java/com/example/UniversityERPSystem/services/UniversityService.java
@@ -1,5 +1,6 @@
 package com.example.UniversityERPSystem.services;
 
+import com.example.UniversityERPSystem.entities.University;
 import com.example.UniversityERPSystem.repositories.UniversityRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,5 +11,12 @@ public class UniversityService {
 
     public UniversityService(UniversityRepository universityRepository) {
         this.universityRepository = universityRepository;
+    }
+
+    public University addUniversity(University university) {
+        if (university == null) {
+            throw new IllegalArgumentException("University cannot be null");
+        }
+        return universityRepository.save(university);
     }
 }
