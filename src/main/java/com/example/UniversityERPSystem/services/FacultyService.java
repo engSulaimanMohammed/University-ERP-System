8263e0d2a67fd8e5c53895cc16fcac8027e9a3c2
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 14:28:23 +0400

    feat: add Faculty create method

--- src/main/java/com/example/UniversityERPSystem/services/FacultyService.java
+++ src/main/java/com/example/UniversityERPSystem/services/FacultyService.java
@@ -1,7 +1,11 @@
 package com.example.UniversityERPSystem.services;
 
+import com.example.UniversityERPSystem.entities.Faculty;
+import com.example.UniversityERPSystem.entities.University;
 import com.example.UniversityERPSystem.repositories.FacultyRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class FacultyService {
@@ -11,6 +15,19 @@ public class FacultyService {
     public FacultyService(FacultyRepository facultyRepository) {
         this.facultyRepository = facultyRepository;
     }
+
+
+    public Faculty addFaculty(Faculty faculty, University university) {
+        if (faculty == null) {
+            throw new IllegalArgumentException("Faculty cannot be null");
+        }
+        faculty.setActive(true);
+        faculty.setCreatedDate(new Date());
+        faculty.setUniversity(university);
+        return facultyRepository.save(faculty);
+    }
+
+
 
 
 }
