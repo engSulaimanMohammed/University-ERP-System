fc61589b65fec3f24333a9e1e9b42f60f76cc7d5
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 18:30:06 +0400

    feat: add final University DTO

--- src/main/java/com/example/UniversityERPSystem/dtos/UniversityDTO.java
+++ src/main/java/com/example/UniversityERPSystem/dtos/UniversityDTO.java
@@ -1,9 +1,14 @@
 package com.example.UniversityERPSystem.dtos;
 
+import com.example.UniversityERPSystem.entities.University;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @Builder
@@ -11,8 +16,32 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UniversityDTO {
 
-    private Long id;
-    private String name;
-    private String locatio
-}
 
+    private Long id;
+
+
+    @NotBlank(message = "University name cannot be blank")
+    @Size(max = 255, message = "University name cannot exceed 255 characters")
+    private String name;
+
+
+    @NotBlank(message = "University location cannot be blank")
+    @Size(max = 255, message = "University location cannot exceed 255 characters")
+    private String location;
+
+
+    public static UniversityDTO convertToDTO(University university) {
+        return UniversityDTO.builder()
+                .id(university.getId())
+                .name(university.getName())
+                .location(university.getLocation())
+                .build();
+    }
+
+
+    public static List<UniversityDTO> convertToDTO(List<University> universities) {
+        return universities.stream()
+                .map(UniversityDTO::convertToDTO)
+                .toList();
+    }
+}
