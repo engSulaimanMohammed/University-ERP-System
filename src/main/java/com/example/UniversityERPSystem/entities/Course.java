53562a7ff4de9ab7637da041f4cb1218bfcd4e14
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 10:02:32 +0400

    feat: add Course entity

--- src/main/java/com/example/UniversityERPSystem/entities/Course.java
+++ src/main/java/com/example/UniversityERPSystem/entities/Course.java
@@ -1,4 +1,17 @@
 package com.example.UniversityERPSystem.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Course {
+
+    private String title;
+    private String courseCode;
+    private int creditHours;
+
+    @ManyToOne
+    private Program program;
+
+
 }
