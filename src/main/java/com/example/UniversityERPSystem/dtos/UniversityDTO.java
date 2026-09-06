a6faeabc73fcd556071b2bdccc458eac6a35e49f
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 18:23:18 +0400

    feat: add University DTO structure

--- src/main/java/com/example/UniversityERPSystem/dtos/UniversityDTO.java
+++ src/main/java/com/example/UniversityERPSystem/dtos/UniversityDTO.java
@@ -1,4 +1,18 @@
 package com.example.UniversityERPSystem.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UniversityDTO {
+
+    private Long id;
+    private String name;
+    private String locatio
 }
+
