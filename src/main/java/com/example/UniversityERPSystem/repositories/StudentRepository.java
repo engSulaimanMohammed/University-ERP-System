6c8f66676ff937306b51eff7ff24232b1adb488e
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 11:43:08 +0400

    feat: add Student repository

--- src/main/java/com/example/UniversityERPSystem/repositories/StudentRepository.java
+++ src/main/java/com/example/UniversityERPSystem/repositories/StudentRepository.java
@@ -1,4 +1,8 @@
 package com.example.UniversityERPSystem.repositories;
 
-public interface StudentRepository {
+
+import com.example.UniversityERPSystem.entities.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
 }
