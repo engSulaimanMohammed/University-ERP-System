79a5816d208ceb918a422c42ea39234d7af0f80b
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 14:32:28 +0400

    feat: add Department service

--- src/main/java/com/example/UniversityERPSystem/services/DepartmentService.java
+++ src/main/java/com/example/UniversityERPSystem/services/DepartmentService.java
@@ -1,4 +1,19 @@
 package com.example.UniversityERPSystem.services;
 
+
+import com.example.UniversityERPSystem.repositories.DepartmentRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class DepartmentService {
+
+    private final DepartmentRepository departmentRepository;
+
+    public DepartmentService(DepartmentRepository departmentRepository) {
+        this.departmentRepository = departmentRepository;
+    }
+
+
+
+
 }
