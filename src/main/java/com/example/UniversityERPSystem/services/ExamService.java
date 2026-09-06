756265a0c312130bcd585fad03ea6e12ee0066d0
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 15:01:46 +0400

    feat: add Exam create method

--- src/main/java/com/example/UniversityERPSystem/services/ExamService.java
+++ src/main/java/com/example/UniversityERPSystem/services/ExamService.java
@@ -1,8 +1,12 @@
 package com.example.UniversityERPSystem.services;
 
 
+import com.example.UniversityERPSystem.entities.Course;
+import com.example.UniversityERPSystem.entities.Exam;
 import com.example.UniversityERPSystem.repositories.ExamRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class ExamService {
@@ -12,6 +16,23 @@ public class ExamService {
     public ExamService(ExamRepository examRepository) {
         this.examRepository = examRepository;
     }
+
+
+
+
+    public Exam addExam(Exam exam, Course course) {
+        if (exam == null) {
+            throw new IllegalArgumentException("Exam cannot be null");
+        }
+        exam.setActive(true);
+        exam.setCreatedDate(new Date());
+        exam.setCourse(course);
+        return examRepository.save(exam);
+    }
+
+
+
+
 
 
 
