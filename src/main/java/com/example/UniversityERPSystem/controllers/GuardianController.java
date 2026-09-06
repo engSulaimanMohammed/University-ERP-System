83aa109273b24b66b80eb6757350f215a752fa40
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 17:15:22 +0400

    feat: add Guardian controller

--- src/main/java/com/example/UniversityERPSystem/controllers/GuardianController.java
+++ src/main/java/com/example/UniversityERPSystem/controllers/GuardianController.java
@@ -1,4 +1,21 @@
 package com.example.UniversityERPSystem.controllers;
 
+import com.example.UniversityERPSystem.entities.Guardian;
+import com.example.UniversityERPSystem.services.GuardianService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/guardian")
 public class GuardianController {
+
+    private final GuardianService guardianService;
+
+    public GuardianController(GuardianService guardianService) {
+        this.guardianService = guardianService;
+    }
+
+
 }
+
