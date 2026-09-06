fa355b8b35f8ac8ae1c2f55e00f5a6db96abcf22
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 10:00:12 +0400

    feat: add Program entity

--- src/main/java/com/example/UniversityERPSystem/entities/Program.java
+++ src/main/java/com/example/UniversityERPSystem/entities/Program.java
@@ -1,4 +1,16 @@
 package com.example.UniversityERPSystem.entities;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Program {
+
+    private String name;
+    private String degreeLevel;
+    private int durationYears;
+
+    @ManyToOne
+    private Department department;
 }
