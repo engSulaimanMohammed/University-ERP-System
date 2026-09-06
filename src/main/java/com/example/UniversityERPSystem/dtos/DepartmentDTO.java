53f7bf4d433982aa3cd7f4dd0bfa44bb1b908b6c
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 18:37:07 +0400

    feat: add Department DTO structure

--- src/main/java/com/example/UniversityERPSystem/dtos/DepartmentDTO.java
+++ src/main/java/com/example/UniversityERPSystem/dtos/DepartmentDTO.java
@@ -1,4 +1,21 @@
 package com.example.UniversityERPSystem.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DepartmentDTO {
+
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private Long facultyId;
 }
