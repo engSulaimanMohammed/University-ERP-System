18dcabe7de34da4024d046be35bf98b0941e5cf1
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 14:56:26 +0400

    feat: add Enrollment service

--- src/main/java/com/example/UniversityERPSystem/services/EnrollmentService.java
+++ src/main/java/com/example/UniversityERPSystem/services/EnrollmentService.java
@@ -1,4 +1,17 @@
 package com.example.UniversityERPSystem.services;
 
+import com.example.UniversityERPSystem.repositories.EnrollmentRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EnrollmentService {
+
+    private final EnrollmentRepository enrollmentRepository;
+
+    public EnrollmentService(EnrollmentRepository enrollmentRepository) {
+        this.enrollmentRepository = enrollmentRepository;
+    }
+
+
+
 }
