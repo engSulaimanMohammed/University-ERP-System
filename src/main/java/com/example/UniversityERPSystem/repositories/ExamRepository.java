226a055a3fec08a2278bf576063767d27fdcff65
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 11:44:30 +0400

    feat: add Exam repository

--- src/main/java/com/example/UniversityERPSystem/repositories/ExamRepository.java
+++ src/main/java/com/example/UniversityERPSystem/repositories/ExamRepository.java
@@ -1,4 +1,7 @@
 package com.example.UniversityERPSystem.repositories;
 
-public interface ExamRepository {
+import com.example.UniversityERPSystem.entities.Exam;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExamRepository extends JpaRepository<Exam, Long> {
 }
