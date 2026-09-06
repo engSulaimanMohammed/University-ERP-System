1da5b1269b2dcbc217ec0443290e6c4e59939eb4
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 15:06:16 +0400

    feat: add Grade create method

--- src/main/java/com/example/UniversityERPSystem/services/GradeService.java
+++ src/main/java/com/example/UniversityERPSystem/services/GradeService.java
@@ -2,8 +2,13 @@ package com.example.UniversityERPSystem.services;
 
 
 
+import com.example.UniversityERPSystem.entities.Enrollment;
+import com.example.UniversityERPSystem.entities.Exam;
+import com.example.UniversityERPSystem.entities.Grade;
 import com.example.UniversityERPSystem.repositories.GradeRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class GradeService {
@@ -13,6 +18,24 @@ public class GradeService {
     public GradeService(GradeRepository gradeRepository) {
         this.gradeRepository = gradeRepository;
     }
+
+
+
+
+    public Grade addGrade(Grade grade, Enrollment enrollment, Exam exam) {
+        if (grade == null) {
+            throw new IllegalArgumentException("Grade cannot be null");
+        }
+        grade.setActive(true);
+        grade.setCreatedDate(new Date());
+        grade.setEnrollment(enrollment);
+        grade.setExam(exam);
+        return gradeRepository.save(grade);
+    }
+
+
+
+
 
 
 
