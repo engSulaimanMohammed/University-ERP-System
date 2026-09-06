7a12f8223277f7c6b6476fbac25592b66cfd0ef6
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 18:52:33 +0400

    feat: add Guardian DTO structure

--- src/main/java/com/example/UniversityERPSystem/dtos/GuardianDTO.java
+++ src/main/java/com/example/UniversityERPSystem/dtos/GuardianDTO.java
@@ -1,4 +1,23 @@
 package com.example.UniversityERPSystem.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class GuardianDTO {
+
+    private Long id;
+
+    private String name;
+
+    private String relationship;
+
+    private String phoneNumber;
+
+    private Long studentId;
 }
