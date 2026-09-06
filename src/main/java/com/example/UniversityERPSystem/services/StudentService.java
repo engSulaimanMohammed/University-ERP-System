db07a3cb7c11b950d4f2676c5d42cd0a59260d02
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 14:53:28 +0400

    feat: add Student create method

--- src/main/java/com/example/UniversityERPSystem/services/StudentService.java
+++ src/main/java/com/example/UniversityERPSystem/services/StudentService.java
@@ -1,7 +1,11 @@
 package com.example.UniversityERPSystem.services;
 
+import com.example.UniversityERPSystem.entities.Program;
+import com.example.UniversityERPSystem.entities.Student;
 import com.example.UniversityERPSystem.repositories.StudentRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class StudentService {
@@ -11,6 +15,26 @@ public class StudentService {
     public StudentService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
+
+
+
+    public Student addStudent(Student student, Program program) {
+        if (student == null) {
+            throw new IllegalArgumentException("Student cannot be null");
+        }
+        student.setActive(true);
+        student.setCreatedDate(new Date());
+        student.setProgram(program);
+        return studentRepository.save(student);
+    }
+
+
+
+
+
+
+
+
 
 
 
