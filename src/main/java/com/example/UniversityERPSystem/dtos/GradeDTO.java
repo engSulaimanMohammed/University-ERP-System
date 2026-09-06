1631019c02487753850388e3c0a57a6ce98dd6ac
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 18:51:22 +0400

    feat: add Grade DTO validation

--- src/main/java/com/example/UniversityERPSystem/dtos/GradeDTO.java
+++ src/main/java/com/example/UniversityERPSystem/dtos/GradeDTO.java
@@ -1,5 +1,11 @@
 package com.example.UniversityERPSystem.dtos;
 
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,11 +19,24 @@ public class GradeDTO {
 
     private Long id;
 
+
+    @PositiveOrZero(message = "Grade score cannot be negative")
     private double score;
 
+
+    @NotBlank(message = "Letter grade cannot be blank")
+    @Size(max = 255, message = "Letter grade cannot exceed 255 characters")
     private String letterGrade;
 
+
+    @NotNull(message = "Enrollment ID cannot be null")
+    @Positive(message = "Enrollment ID must be greater than zero")
     private Long enrollmentId;
 
+
+    @NotNull(message = "Exam ID cannot be null")
+    @Positive(message = "Exam ID must be greater than zero")
     private Long examId;
+
+
 }
