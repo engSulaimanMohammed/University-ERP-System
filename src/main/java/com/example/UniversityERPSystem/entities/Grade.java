239a0be84b689fe4a7f4f4ca84bf278821cd2123
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 10:37:22 +0400

    feat: add Grade entity

--- src/main/java/com/example/UniversityERPSystem/entities/Grade.java
+++ src/main/java/com/example/UniversityERPSystem/entities/Grade.java
@@ -1,4 +1,19 @@
 package com.example.UniversityERPSystem.entities;
 
-public class Grade {
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Grade extends BaseClass{
+
+    private double score;
+    private String letterGrade;
+
+    @ManyToOne
+    private Enrollment enrollment;
+
+    @ManyToOne
+    private  Exam exam;
+
+
 }
