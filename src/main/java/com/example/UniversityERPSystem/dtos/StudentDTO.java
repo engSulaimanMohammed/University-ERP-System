4d99d0b1c0df95cd614f3701cd867d034945a0b5
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 18:46:02 +0400

    feat: add Student DTO validation and protect phone number

--- src/main/java/com/example/UniversityERPSystem/dtos/StudentDTO.java
+++ src/main/java/com/example/UniversityERPSystem/dtos/StudentDTO.java
@@ -1,5 +1,11 @@
 package com.example.UniversityERPSystem.dtos;
 
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,13 +19,32 @@ public class StudentDTO {
 
     private Long id;
 
+
+    @NotBlank(message = "Student name cannot be blank")
+    @Size(max = 255, message = "Student name cannot exceed 255 characters")
     private String name;
 
+
+    @NotBlank(message = "Student gender cannot be blank")
+    @Size(max = 255, message = "Student gender cannot exceed 255 characters")
     private String gender;
 
+
+    // Phone number can be received in requests but will not appear in responses.
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @NotBlank(message = "Student phone number cannot be blank")
+    @Size(max = 255, message = "Student phone number cannot exceed 255 characters")
     private String phoneNumber;
 
+
+    @NotBlank(message = "Student major cannot be blank")
+    @Size(max = 255, message = "Student major cannot exceed 255 characters")
     private String major;
 
+
+    @NotNull(message = "Program ID cannot be null")
+    @Positive(message = "Program ID must be greater than zero")
     private Long programId;
+
+
 }
