96d23c3af71d48775c0364605fc987e294bd2401
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 18:35:49 +0400

    feat: add Faculty entity to DTO conversion

--- src/main/java/com/example/UniversityERPSystem/dtos/FacultyDTO.java
+++ src/main/java/com/example/UniversityERPSystem/dtos/FacultyDTO.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
+import com.example.UniversityERPSystem.entities.Faculty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -31,6 +32,16 @@ public class FacultyDTO {
     @NotNull(message = "University ID cannot be null")
     @Positive(message = "University ID must be greater than zero")
     private Long universityId;
+
+    public static FacultyDTO convertToDTO(Faculty faculty) {
+        return FacultyDTO.builder().id(faculty.getId())
+                .name(faculty.getName()).description(faculty.getDescription())
+                .universityId(faculty.getUniversity() != null ? faculty.getUniversity().getId()
+                                : null).build();
+    }
+
+
+
 
 
 }
