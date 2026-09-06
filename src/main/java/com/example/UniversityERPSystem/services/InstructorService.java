c66cec470694b737a6ff6bd8cbff131047d2f14d
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 14:49:34 +0400

    feat: add Instructor create method

--- src/main/java/com/example/UniversityERPSystem/services/InstructorService.java
+++ src/main/java/com/example/UniversityERPSystem/services/InstructorService.java
@@ -1,8 +1,12 @@
 package com.example.UniversityERPSystem.services;
 
 
+import com.example.UniversityERPSystem.entities.Department;
+import com.example.UniversityERPSystem.entities.Instructor;
 import com.example.UniversityERPSystem.repositories.InstructorRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class InstructorService {
@@ -12,6 +16,22 @@ public class InstructorService {
     public InstructorService(InstructorRepository instructorRepository) {
         this.instructorRepository = instructorRepository;
     }
+
+
+
+    public Instructor addInstructor(Instructor instructor, Department department) {
+        if (instructor == null) {
+            throw new IllegalArgumentException("Instructor cannot be null");
+        }
+        instructor.setActive(true);
+        instructor.setCreatedDate(new Date());
+        instructor.setDepartment(department);
+        return instructorRepository.save(instructor);
+    }
+
+
+
+
 
 
 
