534355970d4cf055005b736f3870bdd1fa9af1cc
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 15:05:27 +0400

    feat: add Grade service

--- src/main/java/com/example/UniversityERPSystem/services/GradeService.java
+++ src/main/java/com/example/UniversityERPSystem/services/GradeService.java
@@ -1,4 +1,21 @@
 package com.example.UniversityERPSystem.services;
 
+
+
+import com.example.UniversityERPSystem.repositories.GradeRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class GradeService {
+
+    private final GradeRepository gradeRepository;
+
+    public GradeService(GradeRepository gradeRepository) {
+        this.gradeRepository = gradeRepository;
+    }
+
+
+
+
 }
+
