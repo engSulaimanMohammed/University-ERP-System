a3cc6c7684b9532414476c54baa124e567cc74a4
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 11:15:42 +0400

    fix: prevent Program Student cyclic reference

--- src/main/java/com/example/UniversityERPSystem/entities/Student.java
+++ src/main/java/com/example/UniversityERPSystem/entities/Student.java
@@ -1,6 +1,7 @@
 package com.example.UniversityERPSystem.entities;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
@@ -16,6 +17,7 @@ public class Student extends BaseClass {
     private String phoneNumber;
     private String major;
 
+    @JsonIgnore
     @ManyToOne
     private Program program;
 
