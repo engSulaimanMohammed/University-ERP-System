7a6d47f40a1aed9f777e89c01088753c5e168317
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 10:26:35 +0400

    feat: add Student entity

--- src/main/java/com/example/UniversityERPSystem/entities/Student.java
+++ src/main/java/com/example/UniversityERPSystem/entities/Student.java
@@ -1,4 +1,18 @@
 package com.example.UniversityERPSystem.entities;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Student {
+
+
+    private String name;
+    private String gender;
+    private String phoneNumber;
+    private String major;
+
+    @ManyToOne
+    private Program program;
 }
