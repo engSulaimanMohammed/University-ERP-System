be11240466663ecb42f3ca0df53830213940c76a
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 11:47:11 +0400

    feat: add Classroom repository

--- src/main/java/com/example/UniversityERPSystem/repositories/ClassroomRepository.java
+++ src/main/java/com/example/UniversityERPSystem/repositories/ClassroomRepository.java
@@ -1,4 +1,7 @@
 package com.example.UniversityERPSystem.repositories;
 
-public interface ClassroomRepository {
+import com.example.UniversityERPSystem.entities.Classroom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClassroomRepository extends JpaRepository<Classroom, Long> {
 }
