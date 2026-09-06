0785e47a525854464825e184edefc9e92b110487
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 14:36:23 +0400

    feat: add Program service

--- src/main/java/com/example/UniversityERPSystem/services/ProgramService.java
+++ src/main/java/com/example/UniversityERPSystem/services/ProgramService.java
@@ -1,4 +1,18 @@
 package com.example.UniversityERPSystem.services;
+import com.example.UniversityERPSystem.repositories.ProgramRepository;
+import org.springframework.stereotype.Service;
 
+
+
+@Service
 public class ProgramService {
+
+    private final ProgramRepository programRepository;
+
+    public ProgramService(ProgramRepository programRepository) {
+        this.programRepository = programRepository;
+    }
+
+
+
 }
