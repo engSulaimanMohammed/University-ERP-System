ba7732d0a66e88de3f313b87cf203924e3d4c6f2
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-06 19:39:46 +0400

    feat: finalize Exam repository with course query

--- src/main/java/com/example/UniversityERPSystem/repositories/ExamRepository.java
+++ src/main/java/com/example/UniversityERPSystem/repositories/ExamRepository.java
@@ -3,5 +3,10 @@ package com.example.UniversityERPSystem.repositories;
 import com.example.UniversityERPSystem.entities.Exam;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ExamRepository extends JpaRepository<Exam, Long> {
+
+    List<Exam> findByCourse_IdAndIsActiveTrue(Long courseId);
+
 }
